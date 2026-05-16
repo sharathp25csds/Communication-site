1f60d0a12fc1d424c77dc4359cd4fd203fb815ea
sharath <sharathp25csds@rnsit.ac.in>
2026-05-16 17:35:52 +0530

    fix port for railway

--- api/index.js
+++ api/index.js
@@ -1,28 +1,12 @@
-// const app = require('../backend/app');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 5000;
-
-// // For Vercel, we export the app
-// if (process.env.NODE_ENV !== 'production') {
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
-
-// module.exports = app;
 const app = require('../backend/app');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
-
